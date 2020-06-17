7f6fd3673a0e47a3563c6d35935abe29f75eed2d
four20 <supryn88@gmail.com>
2020-06-17 22:58:18 +0200

    Polished layout files in app module

--- javaJokes/src/main/java/com/udacity/android/javajokes/data/JokeGenerator.java
+++ javaJokes/src/main/java/com/udacity/android/javajokes/data/JokeGenerator.java
@@ -29,6 +29,14 @@ public final class JokeGenerator {
 
         jokes.add(createJoke(JokeConstants.JOKE_1, JokeConstants.JOKE_1_PUNCH));
         jokes.add(createJoke(JokeConstants.JOKE_2, JokeConstants.JOKE_2_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_3, JokeConstants.JOKE_3_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_4, JokeConstants.JOKE_4_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_5, JokeConstants.JOKE_5_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_6, JokeConstants.JOKE_6_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_7, JokeConstants.JOKE_7_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_8, JokeConstants.JOKE_8_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_9, JokeConstants.JOKE_9_PUNCH));
+        jokes.add(createJoke(JokeConstants.JOKE_10, JokeConstants.JOKE_10_PUNCH));
 
         return jokes;
     }
